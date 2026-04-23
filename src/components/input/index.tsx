f60a7b49bfcff0072dd0ae6feb6103517940de16
Camiloruas <miloruas@gmail.com>
2026-04-23 20:36:00 -0300

    feat: enhance Input component to accept props for better customization

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,9 +1,13 @@
-export function Input() {
+import type { InputHTMLAttributes } from "react";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input(props: InputProps) {
   return (
     <input
       type="text"
-      placeholder="Digite alguma coisa"
       className="border-0 h-9 rounded-md outline-none px-2 mb-3"
+      {...props}
     />
   );
 }
