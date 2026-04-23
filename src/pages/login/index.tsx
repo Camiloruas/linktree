9832bdeff795fb07d2feff721db451f96674af78
Camiloruas <miloruas@gmail.com>
2026-04-23 20:50:21 -0300

    feat: implement login functionality with email and password handling

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
 import { Input } from "../../components/input";
+import { useState, type FormEvent } from "react";
 export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log({
+      email: email,
+      password: password,
+    });
+  }
+
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
@@ -10,8 +22,22 @@ export function Login() {
         </h1>
       </Link>
 
-      <form className="w-full max-w-xl flex flex-col px-2">
-        <Input />
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-xl flex flex-col px-2"
+      >
+        <Input
+          placeholder="Digite o E-mail"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="***************"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button
           type="submit"
           className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white "
