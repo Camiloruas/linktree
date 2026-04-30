b4cb58c0f887efd3878f124c1539354a043cb5a3
Camiloruas <miloruas@gmail.com>
2026-04-30 13:56:08 -0300

    feat: implement link management functionality with add and delete options

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,17 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Input } from "../../components/input";
 import { FiTrash } from "react-icons/fi";
+import { addDoc, collection, onSnapshot, query, orderBy, doc, deleteDoc } from "firebase/firestore";
+import { db } from "../../services/firebaseConnections";
+
+interface LinkProps {
+  id: string;
+  nome: string;
+  url: string;
+  bg: string;
+  color: string;
+}
 
 export function Admin() {
   const [nomeInput, setNomeInput] = useState("");
   const [urlInput, setUrlInput] = useState("");
   const [textColorInput, setTextColorInput] = useState("#f1f1f1");
   const [backgroundColorInput, setBackgroundColorInput] = useState("#121212");
+
+  useEffect(() => {
+    const linksRef = collection(db, "links");
+    const queryRef = query(linksRef, orderBy("created", "asc"));
+
+    const unsub = onSnapshot(queryRef, (onSnapshot) => {
+      const lista = [] as LinkProps[];
+
+      onSnapshot.forEach((doc) => {
+      lista.push()  
+      });
+    });
+  }, []);
+
+  function handleRegister(e: FormEvent) {
+    e.preventDefault();
+    if (nomeInput === "" || urlInput === "") {
+      alert("Preecha todos os campos");
+      return;
+    }
+    try {
+      addDoc(collection(db, "links"), {
+        nome: nomeInput,
+        url: urlInput,
+        bg: backgroundColorInput,
+        color: textColorInput,
+        created: new Date(),
+      });
+      setNomeInput("");
+      setUrlInput("");
+      console.log("cadastrado com Sucesso");
+    } catch (error) {
+      console.log("Erro ao cadastrar no Banco", error);
+    }
+  }
+
   return (
     <div className="flex  items-center flex-col min-h-screen pb-7 px-2">
       <Header />
-      <form className="flex flex-col mt-3 mb-3  w-full max-w-xl ">
+      <form
+        className="flex flex-col mt-3 mb-3  w-full max-w-xl "
+        onSubmit={handleRegister}
+      >
         <label className="text-white font-medium mt-2 mb-2 ">Nome do Link</label>
         <Input
           placeholder="Digite o nome do Link"
@@ -70,16 +119,15 @@ export function Admin() {
       <h2 className="font-bold text-white mb-4 text-2xl">Meus Links</h2>
       <article
         className="flex items-center justify-between w-11/12 max-w-xl rouned py-3 px-2 mb-2"
-        style={{ backgroundColor: "#fff", color: "#000" }}
+        style={{ backgroundColor: "#2563eb", color: "#fff" }}
       >
         <p>Canal do Youtube</p>
         <div>
-          <button>
+          <button className="border border-dashed p-1 rounded ">
             <FiTrash
               size={18}
               color="#fff"
             />
-            .
           </button>
         </div>
       </article>
